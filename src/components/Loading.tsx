034f705dca62a2069e96df6d8a9c66c81f079fee
DavidDeadly <jdrueda513@gmail.com>
2023-05-06 15:01:30 -0500

    feat: Loadign component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import { View, ViewStyle, ActivityIndicator } from 'react-native';
+
+import { COLORS } from '@/constants';
+
+export const Loading: FC<{ styles: ViewStyle }> = ({ styles }) => {
+  return (
+    <View style={styles}>
+      <ActivityIndicator size="large" color={COLORS.primary} />
+    </View>
+  );
+};
