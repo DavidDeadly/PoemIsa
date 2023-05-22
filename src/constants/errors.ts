735705377605328101ed55982a035e36b93a560b
DavidDeadly <jdrueda513@gmail.com>
2023-05-22 18:03:47 -0500

    fix: add AUTH_NETWORK_ERROR to login view

--- src/constants/errors.ts
+++ src/constants/errors.ts
@@ -1,7 +1,9 @@
 export const ERRORS = {
   SING_IN: {
     NETWORK_ERROR: 'NETWORK_ERROR',
-    ACTION_CANCELLED: 'Sign in action cancelled'
+    ACTION_CANCELLED: 'Sign in action cancelled',
+    AUTH_NETWORK_ERROR:
+      '[auth/network-request-failed] A network error (such as timeout, interrupted connection or unreachable host) has occurred.'
   },
   UNEXPECTED: 'Unexpected error'
 };
@@ -9,5 +11,6 @@ export const ERRORS = {
 export const ERRORS_MAP_TO_USER = new Map([
   [ERRORS.SING_IN.NETWORK_ERROR, 'Parece que estas sin conexión'],
   [ERRORS.SING_IN.ACTION_CANCELLED, 'Se canceló el inicio de sesión'],
-  [ERRORS.UNEXPECTED, 'Ha ocurrido un error inesperado']
+  [ERRORS.UNEXPECTED, 'Ha ocurrido un error inesperado'],
+  [ERRORS.SING_IN.AUTH_NETWORK_ERROR, 'Falló la petición de inicio de sesión']
 ]);
