88364a7dafb6d6728a12b1a38649af571872a0c1
DavidDeadly <jdrueda513@gmail.com>
2023-06-21 02:43:35 -0500

    Implement quill text editor

--- src/components/views/CreatePoem.tsx
+++ src/components/views/CreatePoem.tsx
@@ -11,12 +11,16 @@ export const CreatePoem = () => {
     useNavigation<StackNavigationProp<PoemIsaStackParamList>>();
 
   const goToWrite = () => navigation.navigate(SCREENS.APP.WRITE);
+  const goToCapture = () => navigation.navigate(SCREENS.APP.CAPTURE);
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Create</Text>
       <Button onPress={goToWrite}>
         <Text style={styles.text}>Go to Write</Text>
+      </Button>
+      <Button onPress={goToCapture}>
+        <Text style={styles.text}>Go to Capture</Text>
       </Button>
     </View>
   );
--- src/components/views/WritePoem.tsx
+++ src/components/views/WritePoem.tsx
@@ -1,33 +1,112 @@
-import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import { COLORS } from '@/constants';
+import { useRef } from 'react';
+import { StatusBar, StyleSheet, View } from 'react-native';
+import QuillEditor, { QuillToolbar } from 'react-native-cn-quill';
 
-import { PoemIsaStackParamList } from '@/types/components';
-import { SCREENS } from '@/constants';
-import { Button } from '@/components';
+const contextToolbarOptions = [
+  [{ indent: '-1' }, { indent: '+1' }],
+  ['bold', 'italic', 'underline', 'strike'],
+  [{ script: 'sub' }, { script: 'super' }],
+  ['clean']
+];
+
+const bottomToolbarOptions = [
+  [{ align: [false, 'center', 'right'] }],
+
+  [{ color: [] }, { background: [] }],
+
+  [{ font: [] }],
+  [{ size: ['small', false, 'large'] }]
+];
+
+const customStyles = {
+  toolbar: {
+    provider: (provided: object) => ({
+      ...provided,
+      borderColor: COLORS.MAIN.PRIMARY,
+      backgroundColor: COLORS.MAIN.SECONDARY
+    }),
+    root: (provided: object) => ({
+      ...provided,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }),
+    toolset: {
+      root: (provided: object) => ({
+        ...provided,
+        justifyContent: 'center',
+        alignItems: 'center'
+      })
+    }
+  },
+  separator: (provided: object) => {
+    console.log('separator: ', provided);
+    return {
+      ...provided,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'red',
+      color: 'green'
+    };
+  },
+  selection: {
+    close: {
+      view: (provided: object) => ({
+        ...provided,
+        backgroundColor: COLORS.MAIN.PRIMARY
+      })
+    }
+  }
+};
 
 export const WritePoem = () => {
-  const navigation =
-    useNavigation<StackNavigationProp<PoemIsaStackParamList>>();
-
-  const goToWrite = () => navigation.navigate(SCREENS.APP.CAPTURE);
+  const editorRef = useRef<QuillEditor | null>(null);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Write</Text>
-      <Button onPress={goToWrite}>
-        <Text style={styles.text}>Go to Capture</Text>
-      </Button>
-    </View>
+    <>
+      <View style={styles.container}>
+        <QuillEditor
+          ref={editorRef}
+          style={styles.editor}
+          quill={{
+            placeholder: 'Write something beautiful...',
+            theme: 'snow',
+            modules: {
+              toolbar: contextToolbarOptions
+            }
+          }}
+        />
+        <QuillToolbar
+          editor={editorRef}
+          options={bottomToolbarOptions}
+          theme="light"
+          styles={customStyles}
+        />
+      </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: StatusBar.currentHeight
+    paddingTop: StatusBar.currentHeight || 0
   },
   text: {
     color: '#222'
+  },
+  title: {
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    paddingVertical: 10
+  },
+  editor: {
+    flex: 1,
+    padding: 0,
+    borderColor: 'red',
+    borderWidth: 1,
+    margin: 'auto',
+    backgroundColor: 'white'
   }
 });
